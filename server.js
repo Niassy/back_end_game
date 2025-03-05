3c7b55d26c02555748abe000693c4a2f9494c6d1
Niassy <mamadoujbniassy@gmail.com>
2025-03-05 12:27:41 +0000

    Update server.js

--- server.js
+++ server.js
@@ -1,36 +1,29 @@
-const express = require("express");
-const http = require("http");
-const { Server } = require("socket.io");
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
 
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server, {
-  cors: {
-    origin: "*",
-  },
+const io = socketIo(server);
+
+// Serve static files (HTML, CSS, JS) from the "front_end" directory
+app.use(express.static('front_end'));
+
+// Setup socket connection
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    
+    // Handle incoming messages
+    socket.on('chat message', (msg) => {
+        io.emit('chat message', msg); // Broadcast to all clients
+    });
+
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
 });
 
-// Serve a simple message at "/"
-app.get("/", (req, res) => {
-  res.send("Server is running!");
+// Start the server
+server.listen(3000, () => {
+    console.log('Server is running on http://localhost:3000');
 });
-
-io.on("connection", (socket) => {
-  console.log("A user connected");
-
-  socket.on("chat message", (msg) => {
-    io.emit("chat message", msg);
-  });
-
-  socket.on("disconnect", () => {
-    console.log("User disconnected");
-  });
-});
-
-const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
-
-const cors = require('cors');
-app.use(cors());
