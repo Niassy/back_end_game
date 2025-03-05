8afbf3b94c7b562f52348058cac4c1fba5774bde
Niassy <mamadoujbniassy@gmail.com>
2025-03-05 11:28:05 +0000

    Update server.js

--- server.js
+++ server.js
@@ -1,28 +1,33 @@
-const express = require('express');
-const http = require('http');
-const { Server } = require('socket.io');
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
 
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server);
-
-app.use(express.static('public'));
-
-io.on('connection', (socket) => {
-    console.log('A user connected');
-
-    socket.on('joinRoom', ({ username, roomId }) => {
-        socket.join(roomId);
-        io.to(roomId).emit('message', { username, message: `${username} joined the room!` });
-    });
-
-    socket.on('chatMessage', ({ username, roomId, message }) => {
-        io.to(roomId).emit('message', { username, message });
-    });
-
-    socket.on('disconnect', () => {
-        console.log('A user disconnected');
-    });
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+  },
 });
 
-server.listen(3000, () => console.log('Server running on http://localhost:3000'));
+// Serve a simple message at "/"
+app.get("/", (req, res) => {
+  res.send("Server is running!");
+});
+
+io.on("connection", (socket) => {
+  console.log("A user connected");
+
+  socket.on("chat message", (msg) => {
+    io.emit("chat message", msg);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected");
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
